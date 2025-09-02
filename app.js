a449db790dd9239d25c460e81ad65489ac03464c
Keerthana2228 <keerthanasivasami@gmail.com>
2025-09-02 13:46:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get('/todo', function (req, res) {
 
     .listen(port, function () {
         // Logging to console
-        console.log(`Todostp://0.0.0.0:${port}`)
+        console.log(`Todolist running on http://0.0.0.0:${port}`)
     });
 // Export app
 module.exports = app;
